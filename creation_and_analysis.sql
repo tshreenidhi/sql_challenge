756a1afd49cf7d31d50da37d093d9f10d3294912
shreenidhi.t@gmail.com <shreenidhi@Shreenidhis-MacBook-Air.local>
2024-01-31 20:57:01 -0800

    sql files, diagrams and csv files

---
+++ creation_and_analysis.sql
@@ -0,0 +1,145 @@
+﻿-- DATA ENGINEERING
+--All tables are created. 
+-- All required columns are defined for each table 
+--Columns are set to the correct data type
+--Primary Keys set for each table 
+--Correctly references related tables 
+--Tables are correctly related using Foreign Keys 
+--Correctly uses NOT NULL condition on necessary columns 
+--Accurately defines value length for columns 
+
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(25)   NOT NULL,
+    "dept_name" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(25)   NOT NULL,
+    "title" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" VARCHAR(25)   NOT NULL,
+    "birth_date" VARCHAR(25)   NOT NULL,
+    "first_name" VARCHAR(25)   NOT NULL,
+    "last_name" VARCHAR (25)  NOT NULL,
+    "sex" VARCHAR(25)   NOT NULL,
+    "hire_date" VARCHAR(25)   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+-- The dept_emp table is many-many relationship
+CREATE TABLE "dept_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" VARCHAR(25)   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+-- The dept_manager table is many-many relationship
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR(25)   NOT NULL,
+    "emp_no" int   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+-- DATA ANALYSIS
+-- 1. List the employee number, last name, first name, sex, and salary of each employee.
+Select * from employees;
+select * from salaries;
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+JOIN salaries ON employees.emp_no = salaries.emp_no;
+
+-- 2. List the first name, last name, and hire date for the employees who were hired in 1986
+Select * from employees;
+SELECT first_name, last_name, hire_date
+FROM employees
+where hire_date like '%1986';
+
+-- 3. List the manager of each department along with their department number, department name, employee number, last name, and first name.
+select * from dept_manager;
+select * from departments;
+select * from employees;
+
+SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no,
+employees.last_name, employees.first_name
+FROM dept_manager
+JOIN departments ON dept_manager.dept_no = departments.dept_no
+JOIN employees ON dept_manager.emp_no = employees.emp_no;
+
+-- 4. List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+SELECT dept_emp.dept_no, dept_emp.emp_no,employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+JOIN employees ON dept_emp.emp_no = employees.emp_no;
+
+-- 5. List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' and last_name LIKE 'B%';
+
+-- 6. List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT * FROM departments;
+-- Sales department is d007
+SELECT employees.emp_no, employees.last_name, employees.first_name, 
+departments.dept_name
+FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_no = 'd007';
+
+--7. List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT * FROM departments;
+-- Sales department is d007 and Development is d005
+SELECT employees.emp_no, employees.last_name, employees.first_name, 
+departments.dept_name
+FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_no in ('d005','d007');
+
+--8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name)
+
+SELECT last_name, COUNT(last_name) AS "Frequency count"
+FROM employees
+GROUP BY last_name
+ORDER BY "Frequency count" DESC;
